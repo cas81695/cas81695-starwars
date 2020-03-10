014c9a9e055ea25b43879e366bd9b18475f4c4d9
Christian Sanchez <cas81695@gmail.com>
2020-03-10 18:41:14 -0400

    edits on layout

--- server.js
+++ server.js
@@ -3,58 +3,40 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+// Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const characters = [
-    {
-        routeName: 'yoda',
-        name: "Yoda",
-        role: "Jedi Master",
-        age: 900,
-        forcePoints: 2000
-    },
-    {
-        routeName: 'obiwankenobi',
-        name: "Obi Wan Kenobi",
-        role: "Jedi Master",
-        age: 55,
-        forcePoints: 1800
-    },
-    {
-        routeName: 'darthvader',
-        name: "Darth Vader",
-        role: "Sith Lord",
-        age: 45,
-        forcePoints: 1900
-    },
-    {
-        routeName: 'lukeskywalker',
-        name: "Luke Skywalker",
-        role: "Jedi Knight",
-        age: 22,
-        forcePoints: 1950
-    },
-    {
-        routeName: 'darthmaul',
-        name: "Darth Maul",
-        role: "Sith Lord",
-        age: 200,
-        forcePoints: 1200
-    }
+  {
+    routeName: 'yoda',
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: 'darthmaul',
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: 'obiwankenobi',
+    name: "Obi Wan Kenobi",
+    role: "Jedi",
+    age: 55,
+    forcePoints: 1000
+  }
 ];
 
-app.get("/", (reg, res) => {
-    res.sendFile(path.join(__dirname, "view.html"));
-});
-
-app.get("/", (reg, res) => {
-    res.sendFile(path.join(__dirname, "add.html"));
-});
-
 app.get("/", (req, res) => {
-  res.send("Welcome to the Star Wars Page!");
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "add.html"));
 });
 
 app.get("/api/characters/:character", (req, res) => {
@@ -75,7 +57,7 @@ app.get("/api/characters/:character", (req, res) => {
 app.post("/api/characters", (req, res) => {
   const newCharacter = req.body;
 
-  console.log(newCharacter);
+  newCharacter.routeName = req.body.name.split(" ").join("").toLowerCase();
 
   characters.push(newCharacter);
 
